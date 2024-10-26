232f7bd2fd93486c0977325e87772b2f4d41f1ca
yosef-tutian <yoseftutian@gmail.com>
2024-10-26 19:12:30 +0300

    add error page

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -2,7 +2,11 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  // if (data.isErroe) {
+  //   return <p>{data.message}</p>;
+  // }
+  const events = data.events;
   return (
     <>
       <EventsList events={events} />
@@ -11,3 +15,14 @@ function EventsPage() {
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isErroe: true, message: "Could not fatch events." };
+    throw { message: "Could not fatch events." };
+  } else {
+    return response;
+  }
+}
