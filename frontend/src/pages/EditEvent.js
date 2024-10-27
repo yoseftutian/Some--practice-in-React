4f00892a72e59161a21b5e39cd339ca5e57f0a5e
yosef-tutian <yoseftutian@gmail.com>
2024-10-27 18:52:45 +0200

    use useRouteLoaderData with id

--- frontend/src/pages/EditEvent.js
+++ frontend/src/pages/EditEvent.js
@@ -1,3 +1,7 @@
+import { useRouteLoaderData } from "react-router-dom";
+import EventForm from "../components/EventForm";
 export default function EditEventPage() {
-  return <h1>EditEvent</h1>;
+  const data = useRouteLoaderData("event-detail");
+
+  return <EventForm event={data.event} />;
 }
