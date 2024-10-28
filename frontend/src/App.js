084df3acd964e730638d7017a275b8c4c8d200f8
yosef-tutian <yoseftutian@gmail.com>
2024-10-28 19:42:48 +0200

    edit and delete event in the same method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,12 @@ import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
 } from "./pages/EventDetail";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
+import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
-
+import { action as manipulateEventAction } from "./components/EventForm";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -43,13 +43,14 @@ const router = createBrowserRouter([
               {
                 path: "edit",
                 element: <EditEventPage />,
+                action: manipulateEventAction,
               },
             ],
           },
           {
             path: "new",
             element: <NewEventPage />,
-            action: newEventAction,
+            action: manipulateEventAction,
           },
         ],
       },
--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,3 +1,5 @@
+import { json, redirect } from "react-router-dom";
+
 import {
   Form,
   useActionData,
@@ -19,7 +21,7 @@ function EventForm({ method, event }) {
   }
 
   return (
-    <Form method="POST" className={classes.form}>
+    <Form method={method} className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
         <input
@@ -73,3 +75,45 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+  const method = request.method;
+  const data = await request.formData();
+  {
+    data && data.error && (
+      <ul>
+        {Object.values(data.errors).map((err) => (
+          <li key={err}>{err}</li>
+        ))}
+      </ul>
+    );
+  }
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  let url = "http://localhost:8080/events";
+
+  if (method === "PATCH") {
+    const eventId = params.eventId;
+    url = "http://localhost:8080/events/" + eventId;
+  }
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (response.status === 422) {
+    return response;
+  }
+  if (!response.ok) {
+    throw json({ message: "Could not save event." }, { status: 500 });
+  }
+  return redirect("/events");
+}
