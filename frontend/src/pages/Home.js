3a4b607b7fe623ead2d9d84bc4fd740ed9d9e1ff
yosef-tutian <yoseftutian@gmail.com>
2024-10-30 17:33:14 +0200

    test

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,9 @@
+import PageContent from "../components/PageContent";
+
 export default function HomePage() {
-  return <h1>HomePage</h1>;
+  return (
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  );
 }
